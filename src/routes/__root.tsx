835cee4a59ec7388a6cf6f396740463b37c01009
Vansh Mehrotra <vanshmehrotra02@gmail.com>
2026-05-07 16:19:06 +0530

    Set GA4 measurement ID to G-78TPP1B4ET

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -49,10 +49,10 @@ function RootShell({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-78TPP1B4ET" />
         <script
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-XXXXXXXXXX');`,
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-78TPP1B4ET');`,
           }}
         />
       </head>
